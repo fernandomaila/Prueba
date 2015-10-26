091a62ae22b4a8cce6755b49044bf62956e30cfc
fernandomaila <fernando_efml@hotmail.com>
2015-10-26 14:05:21 -0500

    Crear el modelo pokedex

--- Pokedex/api/models/Pokemon.js
+++ Pokedex/api/models/Pokemon.js
@@ -1,14 +1,38 @@
 /**
-* Pokemon.js
-*
-* @description :: TODO: You might write a short summary of how this model works and what it represents here.
-* @docs        :: http://sailsjs.org/#!documentation/models
-*/
+ * Pokemon.js
+ *
+ * @description :: TODO: You might write a short summary of how this model works and what it represents here.
+ * @docs        :: http://sailsjs.org/#!documentation/models
+ */
 
 module.exports = {
 
-  attributes: {
+	attributes: {
+		nombre: {
+			type: 'String',
+			unique: true,
+			required: true
+		}
 
-  }
+		tipo: {
+			type: 'String'
+			enum89: ['agua', 'hierva', 'bicho', 'electrico']
+		}
+		habilidad: {
+			type: 'String',
+			defaultsTo: 'Ninguno'
+		}
+
+		numeroPokemon: {
+			type: 'Integer',
+			required:true
+		}
+
+		fechaVisto: {
+			type: 'String'
+		}
+		
+
+
+	}
 };
-
